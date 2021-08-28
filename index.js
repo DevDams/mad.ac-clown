657e401dd8edea9b7b5db04e6bd5d05b2fb7a085
DevDams <ad.aimedesire@gmail.com>
2021-08-28 21:37:55 +0000

    code refactory, fix error on element zIndex

--- index.js
+++ index.js
@@ -1,14 +1,24 @@
+
+
+// Variables
+let grab = 'grab'
+let grabbing = 'grabbing'
+let index = 0
+
 // Drag function
 function dragBox(el) {
   let pos1 = 0
   let pos2 = 0
   let pos3 = 0
   let pos4 = 0
-  if(el) {
+
+  if (el) {
       el.onmousedown = dragMouseDown
   } else {
       el.onmousedown = dragMouseDown
   }
+
+  el.style.cursor = grabbing
 
   function dragMouseDown(e) {
       e = e || window.event
@@ -28,35 +38,104 @@ function dragBox(el) {
       pos4 = e.clientY
       el.style.top = (el.offsetTop - pos2) + 'px'
       el.style.left = (el.offsetLeft - pos1) + 'px'
-      if (el.offsetTop + 100 > 580) {
-          el.style.transform = 'scale(0.7)'
-      } else if (el.offsetTop + 100 < 580) {
-          el.style.transform = 'scale(1)'
-      }
   }
   
   function closeDragElement() {
-      if (el.offsetTop + 100 > 580) {
-          el.style.display = 'none'
-      } else if (el.offsetTop + 100 < 580) {
-          el.style.display = 'block'
-      }
       document.onmouseup = null
       document.onmousemove = null
   }
 }
 
-dragBox(document.querySelector('#drag'))
-dragBox(document.querySelector('#drag2'))
-dragBox(document.querySelector('#drag3'))
-dragBox(document.querySelector('#drag4'))
-dragBox(document.querySelector('#drag5'))
-dragBox(document.querySelector('#drag6'))
-dragBox(document.querySelector('#drag7'))
-dragBox(document.querySelector('#drag8'))
+// Drag element when mouse down on it
+function drag (el) {
+    el.addEventListener('mousedown', () => {
+        el.style.cursor = grab
+        window.addEventListener('mousemove', dragBox(el))
+        el.style.zIndex = index + 1
+        console.log(window.getComputedStyle(el).zIndex)
+    })
+}
 
-dragBox(document.querySelector('#dragBtn_1'))
-dragBox(document.querySelector('#dragBtn_2'))
-dragBox(document.querySelector('#dragBtn_3'))
-dragBox(document.querySelector('#dragBtn_4'))
-dragBox(document.querySelector('#dragBtn_5'))
+// Stop drag when mouse up on an element
+function dragEl (el) {
+    window.addEventListener('mouseup', (e) => {
+        el.style.cursor = grab
+        window.removeEventListener('mousemove', dragBox(el))
+        el.style.zIndex -= 1
+        console.log(window.getComputedStyle(el).zIndex)
+    })
+}
+
+
+
+// Function to place a box on top of each other if click on it
+drag(document.querySelector('#drag'))
+dragEl(document.querySelector('#drag'))
+
+
+drag(document.querySelector('#drag2'))
+dragEl(document.querySelector('#drag2'))
+
+
+drag(document.querySelector('#drag3'))
+dragEl(document.querySelector('#drag3'))
+
+
+drag(document.querySelector('#drag4'))
+dragEl(document.querySelector('#drag4'))
+
+
+drag(document.querySelector('#drag5'))
+dragEl(document.querySelector('#drag5'))
+
+
+drag(document.querySelector('#drag6'))
+dragEl(document.querySelector('#drag6'))
+
+
+drag(document.querySelector('#drag7'))
+dragEl(document.querySelector('#drag7'))
+
+
+drag(document.querySelector('#drag8'))
+dragEl(document.querySelector('#drag8'))
+
+
+// Menu buttons
+
+drag(document.querySelector('#dragBtn_1'))
+dragEl(document.querySelector('#dragBtn_1'))
+
+
+drag(document.querySelector('#dragBtn_2'))
+dragEl(document.querySelector('#dragBtn_2'))
+
+
+drag(document.querySelector('#dragBtn_3'))
+dragEl(document.querySelector('#dragBtn_3'))
+
+
+drag(document.querySelector('#dragBtn_4'))
+dragEl(document.querySelector('#dragBtn_4'))
+
+
+drag(document.querySelector('#dragBtn_5'))
+dragEl(document.querySelector('#dragBtn_5'))
+
+
+
+
+
+// dragBox(document.querySelector('#drag2'))
+// dragBox(document.querySelector('#drag3'))
+// dragBox(document.querySelector('#drag4'))
+// dragBox(document.querySelector('#drag5'))
+// dragBox(document.querySelector('#drag6'))
+// dragBox(document.querySelector('#drag7'))
+// dragBox(document.querySelector('#drag8'))
+
+// dragBox(document.querySelector('#dragBtn_1'))
+// dragBox(document.querySelector('#dragBtn_2'))
+// dragBox(document.querySelector('#dragBtn_3'))
+// dragBox(document.querySelector('#dragBtn_4'))
+// dragBox(document.querySelector('#dragBtn_5'))
